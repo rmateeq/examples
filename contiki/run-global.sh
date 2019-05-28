cf879731ba7960a0711efe954cb711e754e637a6
Adnan Umer <u.adnan@outlook.com>
2019-05-29 00:39:53 +0500

    Create run-global.sh

---
+++ contiki/run-global.sh
@@ -0,0 +1,5 @@
+DIRECTORY=$(cd `dirname $0` && pwd)
+echo $DIRECTORY
+
+cd $DIRECTORY
+python mac_switching/global_cp.py --config config/wilab2/global_cp_config.yaml --nodes config/wilab2/nodes.yaml
